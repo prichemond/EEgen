e7e89aaec17b865539295447fd34bc58be22fb7a
prichemond <pierre.richemond@gmail.com>
2017-01-29 02:19:31 +0100

    adding mne basic example loader

---
+++ mne_add_testfile.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 29 02:16:37 2017
+
+@author: prichemond
+"""
+from mne.datasets import sample
+data_path = sample.data_path()
+raw_fname = data_path + '/MEG/sample/sample_audvis_filt-0-40_raw.fif'
+
+print(raw_fname)
+raw = mne.fiff.Raw(raw_fname) 
+
+print(raw)
