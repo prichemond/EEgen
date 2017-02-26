dbf92494af55f9e8e3a9311bdabdf98c4d86685c
root <pierre.richemond@gmail.com>
2017-02-26 17:37:37 +0000

    Added plot_dataset utility.

--- wavenet/dataloader_multi_wavenet.py
+++ wavenet/dataloader_multi_wavenet.py
@@ -2,9 +2,13 @@ import numpy as np
 import tensorflow as tf
 import os
 import time
+import pickle
+import h5py
 from scipy.io.wavfile import read
 from scipy.io import loadmat
 from scipy.signal import resample, get_window
+
+PROJECTOR_CHANNEL = 4
 
 
 def load_signal(filename):
@@ -51,13 +55,10 @@ def load_filename_as_mat(filename, verbose=False):
             data = data_reduction(data)
             if verbose:
                 print(filename)
-                if (np.count_nonzero(data) < 10) or (np.any(np.std(data, axis=0) < 0.5)):
-                    print('Warning : file %s is all dropout.' % filename)
-
+            return data
         except:
             print('Unable to load mat file ' + filename)
-
-    return data
+            return
 
 
 def load_all_folder_matrices(folder, verbosity=False):
@@ -65,11 +66,17 @@ def load_all_folder_matrices(folder, verbosity=False):
     all_filenames = tf.gfile.Glob(folder)
     number_filenames = len(all_filenames)
     if verbosity:
-        input('Number of files to load : %f' % number_filenames)
+        input('Number of files to load : %s' % number_filenames)
 
     matlist = []
     for files in all_filenames:
-        matlist.append(load_filename_as_mat(files, verbosity))
+        signal = load_filename_as_mat(files, verbosity)
+        if not((np.count_nonzero(signal) < 10) or (np.any(np.std(signal, axis=0) < 0.5))):
+            matlist.append(signal)
+        else:
+            if verbosity:
+                print('File is dropout, skipping...')
+        print(len(matlist))
 
     if verbosity:
         print('Total time elapsed: %.3fs' % (time.time() - starttime))
@@ -78,14 +85,16 @@ def load_all_folder_matrices(folder, verbosity=False):
 
 
 def data_reduction(data):
+    # As a temporary hack before learning 1x1 convolutions for BSS
+    # Averaging spatially is pointless - for now we project.
+    data = data[:, PROJECTOR_CHANNEL]
     # TODO : Try out Blackman-Harris or Chebyshev window resampling.
     data = resample(data, 600 * 256)
-    # Average all channels together - temporary hack, will learn 1x1 conv
-    # instead
-    # Adding comment.
-    data = np.mean(data, axis=1)
     # need to append, mean-remove, normalize and 8-bit quantize
     return data
 
 matpathtrain = '/home/pierre/pythonscripts/AutoRegressive/eegnet/data/train/*.mat'
-load_all_folder_matrices(matpathtrain, verbosity=True)
+alleegs = load_all_folder_matrices(matpathtrain, verbosity=True)
+# Serialize.
+with open('all_eeg_files.pkl', "wb") as pickle_file:
+    pickle.dump(alleegs, pickle_file)
---
+++ wavenet/plot_dataset.py
@@ -0,0 +1,85 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+import os
+from scipy.io import loadmat
+from scipy.signal import resample
+import pandas as pd
+import time
+
+NUM_FIGS = 20
+
+
+# Generate a multi-channel plot from a raw single .mat file.
+def plot_single_matfile(filename, verbose=False):
+    starttime = time.time()
+    try:
+        if os.path.exists(filename):
+            mat = loadmat(filename)
+            data = mat['dataStruct']['data'][0][0]
+            # Resample and plot in figure.
+            data = resample(data, 600 * 256)
+            dataframe = pd.DataFrame(data)
+            # Looks like a bug in the 'colormap' option.
+            # Either the 'colormap' option or the below line won't seem to work.
+            # plt.set_cmap('hot')
+            dataframe.plot(subplots=True, figsize=(48, 36))
+
+            filename_end = filename.split('/')
+            filename_end = filename_end[-1]
+            plt.savefig('./figures/figure_' +
+                        filename_end.split('.')[0] + '.png')
+
+            if verbose:
+                print('Plot saved. Time elapsed: %.2fs' %
+                      (time.time() - starttime))
+
+            return
+        """
+        plt.figure(figsize=(48, 36))
+        plt.title('Training matrix : ' + filename_end)
+        plt.tight_layout()
+        sharedxaxis = np.arange(600 * 256)
+        figs, axes = plt.subplots(nrows=16, ncols=1, sharex=True)
+        for i in range(16):
+            print(data[:, i])
+            axes[i].plot(data[:, i])
+
+                """
+    except:
+        print('File exists, but unable to load mat file ' + filename)
+        return
+
+
+# Generate NUM_FIGS plots from pickled training data, randomly picked.
+def plot_dataset_subset(filename):
+    # Matplotlib style from the book 'Bayesian methods for Hackers'
+    plt.style.use('bmh')
+    try:
+        f = open(filename, 'rb')
+        signal = pickle.load(f)
+        signum = len(signal)
+        permutation = np.random.choice(np.arange(signum), size=NUM_FIGS)
+
+        for i in range(NUM_FIGS):
+            index = permutation[i]
+            smallsig = signal[index]
+            # Now onto the plotting & saving.
+            plt.figure(figsize=(48, 18))
+            plt.title('Training data number : ' + str(index))
+            plt.plot(smallsig)
+            plt.savefig('./figures/figure_' + str(index) + '.png')
+            # Adding basic stats.
+            print(index)
+            print(np.min(smallsig))
+            print(np.max(smallsig))
+            print(smallsig[0:1024])
+
+    except:
+        print('Unable to open file path, aborting.')
+
+    return
+
+# plot_dataset_subset('./all_eeg_files.pkl')
+train_path = '/home/pierre/pythonscripts/AutoRegressive/eegnet/data/train/'
+plot_single_matfile(train_path + '1_531_0.mat', verbose=True)
